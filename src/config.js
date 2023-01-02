5e85729cda28aed70c908fac7db0f1216c16cd48
HariprakashM <hariprakashm20@gmail.com>
2023-01-02 09:21:51 +0530

    vercel

--- src/config.js
+++ src/config.js
@@ -1,4 +1,5 @@
 export const config={
-    api:"https://pixel-stream.herokuapp.com"
+    api:"https://pixel-stream-backend.vercel.app"
+    // api:"https://pixel-stream.herokuapp.com"
     // api:"http://localhost:5000"
 }
