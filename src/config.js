ef6ec1258d26cafaf76d4a3f080f3f39ba008b09
Hariprakash-cs <hari.prakash@contentstack.com>
2025-10-22 17:04:13 +0530

    latest changes

--- src/config.js
+++ src/config.js
@@ -1,9 +1,9 @@
 // Contentstack CMS Configuration
 export const config = {
   contentstack: {
-    apiKey: "blt2920829618faa573",
-    accessToken: "cs735d2fa1012c15bdfc708585",
-    environment: "production",
-    region: "us",
+    apiKey: process.env.REACT_APP_CONTENTSTACK_API_KEY,
+    accessToken: process.env.REACT_APP_CONTENTSTACK_ACCESS_TOKEN,
+    environment: process.env.REACT_APP_CONTENTSTACK_ENVIRONMENT || "production",
+    region: process.env.REACT_APP_CONTENTSTACK_REGION || "us",
   },
 };
